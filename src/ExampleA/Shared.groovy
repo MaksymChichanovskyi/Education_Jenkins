6ef40514610a5c677e9cbe50e171a91365cc3e14
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-11 15:21:10 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -1,3 +1,11 @@
 package ExampleA
+ checkout changelog: false, scm: [$class: 'GitSCM', branches: [[name: '*/main']],
+                                             extensions       : [[$class: 'LocalBranch', localBranch: '**'], [$class: 'UserIdentity', name: 'Jenkins']],
+                                             userRemoteConfigs: [[credentialsId: 'cf84bbaf-792c-4bac-98ae-b80958b2656f', refspec: '+refs/heads/main:refs/remotes/origin/main',
+                                             url: 'https://github.com/MaksymChichanovskyi/Education_Jenkins.git']]]
 
+
+def imageName = "maven:3.9.8-amazoncorretto-11"
+        docker.image(imageName).pull()
+        docker.image(imageName).inside()
 return this 
