a8add61229e72d433c873fa8dc0744c8bde62757
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-12 12:40:16 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -2,7 +2,8 @@ package ExampleA
 
 def call (String imageName){
 docker.image(imagename).pull()
-docker.image(imagename).inside()
+docker.image(imagename).inside(){
 sh "maven clean package"
+}
 }
 return this 
