0f2872e667b1271a3e83508a54089284db47767d
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-12 14:03:56 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -1,11 +1,10 @@
 package ExampleA
 
-class InstalMaven{
-static void installMaven (String imageName){
+
+void installMaven (String imageName){
 docker.image(imagename).pull()
 docker.image(imagename).inside(){
 sh "maven clean package"
       }
     }
-}
 return this 
